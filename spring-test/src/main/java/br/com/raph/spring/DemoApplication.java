1f9b799a4a419fc4d789b82ba141bbe608894ea9
administrator02 <rafael.araujo@varitus.com.br>
2016-09-20 22:36:31 -0300

    Configurado para gerar war e jar

---
+++ spring-test/src/main/java/br/com/raph/spring/DemoApplication.java
@@ -0,0 +1,39 @@
+package br.com.raph.spring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.raph.spring.rest.client.Quote;
+
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+public class DemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder).sources(DemoApplication.class);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			System.err.println( quote.toString() );
+		};
+	}
+}
--- spring-test/src/main/java/br/com/raph/spring/rest/client/ClientApplication.java
+++ spring-test/src/main/java/br/com/raph/spring/rest/client/ClientApplication.java
@@ -9,26 +9,12 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ClientApplication {
 	
-	private static final Logger log = LoggerFactory.getLogger(ClientApplication.class);
 	
 //	public static void main(String[] args) {
 //		SpringApplication.run(ClientApplication.class, args);
 //	}
 	
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-	
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Quote quote = restTemplate.getForObject(
-					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-			log.info(quote.toString());
-		};
-	}
 }
